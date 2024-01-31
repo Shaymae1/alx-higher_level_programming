dc1557452771c55ef6246dd667238ea5cd3ddb05
Shaymae1 <chaymaeelkarcha@gmail.com>
2024-01-30 16:33:09 -0800

    last

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-let def = {};
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const abc = JSON.parse(body);
-    abc.characters.forEach(function (item, index, array) {
-      request(item, function (error, response, content) {
-        if (error) {
-          console.log(error);
-        } else {
-          def = JSON.parse(content);
-          console.log(def.name);
-        }
-      });
-    });
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const { argv } = require('process');
 
-const BaseUrl = 'https://swapi-api.hbtn.io/api';
-request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
-  if (error) {
-    console.error(error);
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
-  console.log(JSON.parse(body).title);
 });
